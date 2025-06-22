709539078dd1676f9a899a0f00fb9aa40659da15
DP <102902256+defeatedperson@users.noreply.github.com>
2025-06-22 20:20:37 +0800

    Update footer.php修复跳转问题

--- dc-admin/footer.php
+++ dc-admin/footer.php
@@ -4,9 +4,9 @@
     <div class="footer">
         <p class="footer-container">
             <span>© 2025 云阶星幕工作室</span>
-            <a href="https://github.com/defeatedperson/xcc" class="footer-link">项目地址</a>
-            <a href="https://xcdream.com/" class="footer-link">官网</a>
-            <a href="https://re.xcdream.com/zhichiwtx" class="footer-link">支持我们</a>
+            <a href="https://github.com/defeatedperson/xcc" target="_blank class="footer-link">项目地址</a>
+            <a href="https://xcdream.com/" target="_blank class="footer-link">官网</a>
+            <a href="https://re.xcdream.com/zhichiwtx" target="_blank class="footer-link">支持我们</a>
         </p>
     </div>
 </body>
